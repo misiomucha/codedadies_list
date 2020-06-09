e08fbb0def2aedc843af22733e14acb5be8bb1da
misiomucha <kilagm86@gmail.com>
2020-06-09 10:44:51 +0200

    add new search

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, template_name='base.html')
+
+def new_search(request):
+    return render(request, 'my_app/new_search.html')
